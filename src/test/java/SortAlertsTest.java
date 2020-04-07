663679e62177b6e4f1c571b78165cc97164ca5e0
NataliaSzczepan <59261950+NSzczepan@users.noreply.github.com>
2020-04-07 16:24:34 +0200

    Modified file

--- src/test/java/SortAlertsTest.java
+++ src/test/java/SortAlertsTest.java
@@ -14,23 +14,25 @@ public class SortAlertsTest extends TestBase {
         driver.findElement(By.id("inputUsername")).sendKeys("test1234");
         driver.findElement(By.id("inputPassword")).sendKeys("test1234");
         driver.findElement(By.cssSelector(".btn")).click();
-
+        Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/app/div[2]/div[2]/ul/li[2]/a")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[2]/div/button[1]")).click();
-        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[2]/div/div/button[4]")).click();
-
+        Thread.sleep(2000);
         WebElement dropdownButton = driver.findElement(By.id("dropdownButton"));
         dropdownButton.click();
-        WebElement emailOption = driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[3]/div[1]/div/div/a[5]"));
+        Thread.sleep(2000);
+        WebElement emailOption = driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[5]/div[1]/div/div/a[5]"));
         emailOption.click();
-
-       driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[3]/div[1]/button/span")).click();
-       Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[3]/div[2]/button[1]")).click();
         Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[3]/div[1]/button/span")).click();
+        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[5]/div[2]/button[1]")).click();
         Thread.sleep(2000);
-        Assert.assertTrue(driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[1]/h1")).getText().equals("My Alerts"));
+        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[5]/div[1]/button/span")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[5]/div[1]/button/span")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[5]/div[2]/button[2]")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/h1")).getText().equals("My Alerts"));
 
     }
 }
