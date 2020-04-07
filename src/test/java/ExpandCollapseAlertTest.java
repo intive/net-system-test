9d0cf9fe52cd1bbee4d3a3ef86f8a66bcbd1d7b8
NataliaSzczepan <59261950+NSzczepan@users.noreply.github.com>
2020-04-07 16:26:07 +0200

    Modified file

--- src/test/java/ExpandCollapseAlertTest.java
+++ src/test/java/ExpandCollapseAlertTest.java
@@ -13,24 +13,21 @@ public class ExpandCollapseAlertTest extends TestBase {
         driver.findElement(By.cssSelector(".btn")).click();
         driver.findElement(By.xpath("/html/body/app/div[2]/div[2]/ul/li[2]/a")).click();
         driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[2]/div/button[1]")).click();
-        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[3]/div[2]/button[1]")).click();
-        Thread.sleep(200);
+
+      /* driver.findElement(By.xpath("\"/html/body/app/div[3]/div[2]/div[5]/div[2]/button[1]")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[4]/div[1]/div[2]/button[1]/span")).click();
 
-        //driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/table/tbody/tr[1]/td[5]/button")).click();
-        // driver.findElement(By.id("condition")).click();
-        //Select options = new Select(driver.findElement(By.tagName("select")));
-        //options.selectByVisibleText("Crossing");
         Select size = new Select(driver.findElement(By.id("valueType")));
         size.selectByVisibleText("Price");
         driver.findElement(By.id("value")).clear();
         driver.findElement(By.id("value")).sendKeys("200");
         driver.findElement(By.id("innerDiv")).click();
         driver.findElement(By.cssSelector("#innerDiv > div.text-right > button.btn.btn-success.btn-sm")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[3]/div[2]/button[1]")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[3]/div[3]/div[2]/button[2]")).click();
+        Thread.sleep(1000);*/
+        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[5]/div[2]/button[1]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/app/div[3]/div[2]/div[5]/div[2]/button[2]")).click();
         Thread.sleep(1000);
         //driver.findElement(By.cssSelector("#innerDiv > div.text-right > button.btn.btn-success.btn-sm")).click();
 
