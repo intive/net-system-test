07c199f80fc323d655727f9c7447296d460a60b0
NataliaSzczepan <59261950+NSzczepan@users.noreply.github.com>
2020-05-03 22:12:05 +0200

    Updated Create Account Test

--- src/test/java/CreateAccountTest.java
+++ src/test/java/CreateAccountTest.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import selenium.base.TestBase;
 import selenium.pages.CreateAccountPage;
@@ -5,9 +7,8 @@ import selenium.pages.CreateAccountPage;
 public class CreateAccountTest extends TestBase{
 
       @Test
-
     public  void testCreateAccountPage() throws InterruptedException {
-          CreateAccountPage createAccount = new CreateAccountPage(driver);
+        CreateAccountPage createAccount = new CreateAccountPage(driver);
         createAccount.clickCreateAccount();
         createAccount.createAccount("test", "testpatron2020@o2.pl", "test", "test");
         createAccount.clickCreate();
