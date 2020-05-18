054ec38554ea553a172284ad71ebdf85e1febc9a
Agnieszka Surmiak <surmiak.aga@gmail.com>
2020-05-18 02:45:42 +0200

    Branch before Demo

--- src/main/java/selenium/pages/IndicatorsViewPage.java
+++ src/main/java/selenium/pages/IndicatorsViewPage.java
@@ -1,14 +1,9 @@
 package selenium.pages;
-<<<<<<< HEAD
-=======
-
->>>>>>> Branch after changes before demo
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class IndicatorsViewPage {
 
-<<<<<<< HEAD
     private WebDriver driver;
     private By btcButton = By.cssSelector(".table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2) > a:nth-child(1)");
     private By indicatorsButton = By.cssSelector("div.btn-group:nth-child(2) > button:nth-child(4)");
@@ -29,28 +24,6 @@ public class IndicatorsViewPage {
     private By thirtyTimeframe = By.cssSelector("button.btn:nth-child(9)");
     public By rsiChart = By.id("rsi_chart_div");
     public By smaChart = By.id("sma_chart_div");
-=======
-    public WebDriver driver;
-    public By btcButton = By.cssSelector(".table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2) > a:nth-child(1)");
-    public By indicatorsButton = By.cssSelector("div.btn-group:nth-child(2) > button:nth-child(4)");
-    public By rsiButton = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(1) > button:nth-child(2)");
-    public By rsiLabel = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > a:nth-child(1)");
-    public By smaButton = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(1) > button:nth-child(3)");
-    public By smaHighPriceButton = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(2)");
-    public By smaLowPriceButton = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(3)");
-    public By smaOpenPriceButton = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(4)");
-    public By smaClosePriceButton = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(5)");
-    public By twoTimeframe = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(2)");
-    public By threeTimeframe = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(3)");
-    public By fourTimeframe = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(4)");
-    public By fiveTimeframe = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(5)");
-    public By sixTimeframe = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(6)");
-    public By fourteenTimeframe = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(7)");
-    public By twentyTimeframe = By.cssSelector("div.btn-toolbar:nth-child(6) > div:nth-child(2) > button:nth-child(8)");
-    public By thirtyTimeframe = By.cssSelector("button.btn:nth-child(9)");
-    public static By rsiChart = By.id("rsi_chart_div");
-    public static By smaChart = By.id("sma_chart_div");
->>>>>>> Branch after changes before demo
 
     public IndicatorsViewPage(WebDriver driver) { this.driver = driver; }
 
@@ -93,8 +66,4 @@ public class IndicatorsViewPage {
         return driver.findElement(rsiLabel).getText();
     }
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> Branch after changes before demo
